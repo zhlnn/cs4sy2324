bb34d0336319cca354276c9890a6ac322ed84ac1
zhlnn <115552686+zhlnn@users.noreply.github.com>
2023-09-05 14:23:23 +0800

    Update CS4TruthBalas.java

--- src/cs4truthbalas/CS4TruthBalas.java
+++ src/cs4truthbalas/CS4TruthBalas.java
@@ -15,9 +15,22 @@ public class CS4TruthBalas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        String greeting = "Hello World!";
-        System.out.println(greeting + " Hello Pisay!");
+        System.out.println("Student 1");
+        String name1 = "Johnny";
+        String elective1 = "AdChem";
+        double mark1 = 1.25;
+        System.out.println("Name: " + name1 + '\n' + "Elective: " + elective1 + '\n' + "Mark: " + mark1 + '\n');
+        
+        System.out.println("Student 2");
+        String name2 = "Ethan";
+        String elective2 = "LifeSci";
+        double mark2 = 1.50;
+        System.out.println("Name: " + name2 + '\n' + "Elective: " + elective2 + '\n' + "Mark: " + mark2 + '\n');
+        
+        System.out.println("Student 3");
+        String name3 = "Jake";
+        String elective3 = "LabTech";
+        double mark3 = 1.25;
+        System.out.println("Name: " + name3 + '\n' + "Elective: " + elective3 + '\n' + "Mark: " + mark3 + '\n');
     }
-    
 }
