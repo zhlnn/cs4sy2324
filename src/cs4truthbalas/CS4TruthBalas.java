fc1dcce339a017d1ff562c245f6acfb83c160569
zhlnn <115552686+zhlnn@users.noreply.github.com>
2023-09-05 21:37:29 +0800

    Update CS4TruthBalas.java

--- src/cs4truthbalas/CS4TruthBalas.java
+++ src/cs4truthbalas/CS4TruthBalas.java
@@ -15,22 +15,31 @@ public class CS4TruthBalas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Student 1");
-        String name1 = "Johnny";
-        String elective1 = "AdChem";
-        double mark1 = 1.25;
-        System.out.println("Name: " + name1 + '\n' + "Elective: " + elective1 + '\n' + "Mark: " + mark1 + '\n');
-        
-        System.out.println("Student 2");
-        String name2 = "Ethan";
-        String elective2 = "LifeSci";
-        double mark2 = 1.50;
-        System.out.println("Name: " + name2 + '\n' + "Elective: " + elective2 + '\n' + "Mark: " + mark2 + '\n');
-        
-        System.out.println("Student 3");
-        String name3 = "Jake";
-        String elective3 = "LabTech";
-        double mark3 = 1.25;
-        System.out.println("Name: " + name3 + '\n' + "Elective: " + elective3 + '\n' + "Mark: " + mark3 + '\n');
+        System.out.println("Idol 1");
+		String name1 = "Johnny";
+		int age1 = 19;
+		int training1 = 1;
+		System.out.println("Name: " + name1 + '\n' + "Age: " + age1 + '\n' + "Training Years: " + training1 + '\n');
+
+		System.out.println("Idol 2");
+		String name2 = "Ethan";
+		int age2 = 21;
+		int training2 = 3;
+		System.out.println("Name: " + name2 + '\n' + "Age: " + age2 + '\n' + "Training Years: " + training2 + '\n');
+
+		System.out.println("Idol 3");
+		String name3 = "Benjamin";
+		int age3 = 20;
+		int training3 = 2;
+		System.out.println("Name: " + name3 + '\n' + "Age: " + age3 + '\n' + "Training Years: " + training3 + '\n');
+
+		boolean longestTraining = training2 > training1 && training2 > training3;
+		System.out.println("Ethan is the idol with the longest training period: " + longestTraining + '\n');
+
+		boolean sameAge = age1 == age3;
+		System.out.println("Johnny and Benjamin are of the same age: " + sameAge + '\n');
+
+		int ageGap = age2 - age1;
+		System.out.println("Difference in years of Johnny and Ethan's ages: " + ageGap);
     }
 }
